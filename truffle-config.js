559697c6f7426e548b860ade983ab253b262a3dc
Saaraghaa <ssaraga29@gmail.com>
2024-08-10 18:08:36 +0530

    Create truffle-config.js

---
+++ truffle-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1", // Ganache's default address
+      port: 7545, // Ganache's default port
+      network_id: "5777", // Match Ganache's network ID
+    },
+  },
+  compilers: { 
+      solc: {
+        version: "^0.8.0", 
+      }
+    }
+}; 
